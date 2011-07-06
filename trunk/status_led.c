b9e8f41cd73aad4076a5ee4aa63cf63e4aaddf95
Benoit.christiaens@scarlet.be <Benoit.christiaens@scarlet.be@558aef47-f50e-136d-135e-4b59210d2c31>
2011-07-06 04:58:03 +0000

    set back led cycle to 1 second

--- trunk/status_led.c
+++ trunk/status_led.c
@@ -9,8 +9,8 @@ void status_led_init(void)
     PORTSetPinsDigitalOut(IOPORT_F, BIT_1);
     mPORTFClearBits(BIT_1);
 
-    // configure Timer 1 using external asynchronous clock, 1:1 prescale, 0x2000 period
-    OpenTimer1(T1_ON | T1_SOURCE_EXT | T1_SYNC_EXT_OFF | T1_PS_1_1, 0x2000);
+    // configure Timer 1 using external asynchronous clock, 1:1 prescale, 0x4000 period
+    OpenTimer1(T1_ON | T1_SOURCE_EXT | T1_SYNC_EXT_OFF | T1_PS_1_1, 0x4000);
 			//Timer1 On, External source, No synchronization, 1:1 Prescaler, Period
 
     // Set up the timer interrupt with a priority of 2
