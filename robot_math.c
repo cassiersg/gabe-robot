66cd035c0157d92580cb22cd6f7da3d8a54303f9
gaetan.cassiers@gmail.com <gaetan.cassiers@gmail.com@558aef47-f50e-136d-135e-4b59210d2c31>
2011-09-23 19:48:44 +0000

    macros dans robot_math.c

--- robot_math.c
+++ robot_math.c
@@ -2,32 +2,41 @@
 
 /*r_asin, r_acos and r_atan can be negative in order to exprees FAILURE */
 
-uint16 r_sqrt(uint32 x);
+#define COS_SIN_BITS 12
+#define TANGENT_BITS 12
+#define ANGLE_BITS   13
+
+#define COS_SIN_UNIT (1<<COS_SIN_BITS) // 4096
+#define TANGENT_UNIT (1<<TANGENT_BITS) // 4096
+#define ANGLE_UNIT   (1<<ANGLE_BITS)   // 8192
+
+#define SIN_PI4 ((46400*COS_SIN_UNIT)>>16)  // 46400.0>>16 *2*PI >= sin(pi/4)
+
 static int sinCos(int x);
 
 int16 r_asin(int x)
 {
-    if (x>4096 || x< -4096)
+    if (x>COS_SIN_UNIT || x< -COS_SIN_UNIT)
         return FAILURE;
     else
     {
-        if (x>2897 || x<-2897)
+        if (x>SIN_PI4 || x<-SIN_PI4)
         {
-            return x<0 ? r_asin(sinCos(x))-8192/4 : 8192/4-r_asin(sinCos(x));
+            return x<0 ? r_asin(sinCos(x))-ANGLE_UNIT/4 : ANGLE_UNIT/4-r_asin(sinCos(x));
         }
         else
         {
-            int x_square = (x*x)>>12;
+            int x_square = (x*x)>>COS_SIN_BITS;
             int i = 1;
-            int coeff = 1<<12;
-            int res = 1<<12;
+            int coeff = 1<<COS_SIN_BITS;
+            int res = 1<<COS_SIN_BITS;
             for (; i<7; i++)
             {
                 coeff*= (x_square * (2*i-1)) / (2*i);
-                coeff = coeff>>12;
+                coeff = coeff>>COS_SIN_BITS;
                 res+= coeff / (2*i+1);
             }
-            res = (res*x)>>12;
+            res = (res*x)>>COS_SIN_BITS;
             res = ((res<<16) / (R_PIB16));
             return res;
         }
@@ -36,32 +45,32 @@ int16 r_asin(int x)
 
 int16 r_acos(int x)
 {
-   if (x>4096 || x< -4096)
+   if (x>COS_SIN_UNIT || x< -COS_SIN_UNIT)
         return FAILURE;
     else
     {
-        int angle = 8192/4 - r_asin(x);
+        int angle = ANGLE_UNIT/4 - r_asin(x);
         return angle;
     }
 }
 
 int16 r_atan(int x)
 {
-    if (x < -4096 || x > 4096)
+    if (x < -TANGENT_UNIT || x > TANGENT_UNIT)
         return FAILURE;
     else
     {
-        int x_square = (x*x)>>12;
-        int coeff = 1<<12;
-        int res = 1<<12;
+        int x_square = (x*x)>>TANGENT_BITS;
+        int coeff = 1<<TANGENT_BITS;
+        int res = 1<<TANGENT_BITS;
         int i = 1;
-        for (;i<32; i++)
+        for (;i<20; i++)
         {
             coeff*= x_square;
-            coeff = -(coeff>>12);
+            coeff = -(coeff>>TANGENT_BITS);
             res+= coeff / (2*i+1);
         }
-        res = (res*x)>>12;
+        res = (res*x)>>TANGENT_BITS;
         res = ( (res<<16) / (R_PIB16) );
         return res;
     }
@@ -91,5 +100,5 @@ uint16 r_sqrt(uint32 x)
 
 static int sinCos(int x)
 {
-    return r_sqrt(4096*4096-x*x);
+    return r_sqrt(COS_SIN_UNIT*COS_SIN_UNIT-x*x);
 }
